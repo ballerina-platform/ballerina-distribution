f251a1532d639246a9bd5b2fc12241eca5ae5322
Tharik Kanaka <tharik.kanaka@gmail.com>
2019-12-12 14:51:07 +0530

    Fix indentation formatting

--- resources/tools/bin/ballerina.bat
+++ resources/tools/bin/ballerina.bat
@@ -40,21 +40,21 @@ if "%dist%" == "true" (
         rd /s /q %CURRENT_PATH%\..\ballerina-command-tmp
    )
 ) else (
-	set BALLERINA_HOME=
-	for /f %%a in (%CURRENT_PATH%\..\distributions\ballerina-version) do (
-		set BALLERINA_HOME=%%a
-	)
-	if exist %userprofile%\.ballerina\ballerina-version (
-	   set "FILE_PATH=%userprofile%\.ballerina\ballerina-version"
-	)
+    set BALLERINA_HOME=
+    for /f %%a in (%CURRENT_PATH%\..\distributions\ballerina-version) do (
+        set BALLERINA_HOME=%%a
+    )
+    if exist %userprofile%\.ballerina\ballerina-version (
+        set "FILE_PATH=%userprofile%\.ballerina\ballerina-version"
+    )
 
-	SetLocal EnableDelayedExpansion
-	for /f %%a in (!FILE_PATH!) do (
-		if exist %%a (
-			set BALLERINA_HOME=%%a
-		)
-	)
-	call %CURRENT_PATH%..\distributions\!BALLERINA_HOME!\bin\ballerina.bat %*
+    SetLocal EnableDelayedExpansion
+    for /f %%a in (!FILE_PATH!) do (
+        if exist %%a (
+            set BALLERINA_HOME=%%a
+        )
+    )
+    call %CURRENT_PATH%..\distributions\!BALLERINA_HOME!\bin\ballerina.bat %*
 )
 set merge=false
 if "%1" == "help" (
