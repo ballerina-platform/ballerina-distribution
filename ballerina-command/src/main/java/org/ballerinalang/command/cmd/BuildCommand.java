f529d9b7ec6150cc5c16d9594f487b3c472c16bc
warunalakshitha <warudanu@gmail.com>
2019-12-17 19:48:24 +0530

    Add notification support for build command

---
+++ ballerina-command/src/main/java/org/ballerinalang/command/cmd/BuildCommand.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ballerinalang.command.cmd;
+
+import org.ballerinalang.command.BallerinaCliCommands;
+import org.ballerinalang.command.util.ToolUtil;
+import picocli.CommandLine;
+
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * This class represents the "build" command and used to notify latest distribution information.
+ */
+@CommandLine.Command(name = "version", description = "Prints Ballerina version")
+public class BuildCommand extends Command implements BCommand {
+
+    public BuildCommand(PrintStream printStream) {
+        super(printStream);
+    }
+
+    @CommandLine.Parameters(description = "Command name")
+    private List<String> buildCommands;
+
+    @CommandLine.Option(names = {"--help", "-h", "?"}, hidden = true)
+    private boolean helpFlag;
+
+    public void execute() {
+        if (helpFlag) {
+            return;
+        }
+        ToolUtil.checkForUpdate(getPrintStream());
+    }
+
+    @Override
+    public String getName() {
+        return BallerinaCliCommands.VERSION;
+    }
+
+    @Override
+    public void printLongDesc(StringBuilder out) {
+
+    }
+
+    @Override
+    public void printUsage(StringBuilder out) {
+    }
+
+    @Override
+    public void setParentCmdParser(CommandLine parentCmdParser) {
+    }
+}
