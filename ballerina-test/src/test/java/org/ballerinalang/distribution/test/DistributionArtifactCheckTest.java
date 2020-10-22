194b2da65ebb0f7f2ae6e9592db15fde62e1815a
anuruddhal <anuruddhal@wso2.com>
2020-10-22 18:31:19 +0530

    Fix azure function version

--- ballerina-test/src/test/java/org/ballerinalang/distribution/test/DistributionArtifactCheckTest.java
+++ ballerina-test/src/test/java/org/ballerinalang/distribution/test/DistributionArtifactCheckTest.java
@@ -282,7 +282,7 @@ public class DistributionArtifactCheckTest {
                 .resolve("bir-cache")
                 .resolve("ballerinax")
                 .resolve("azure.functions")
-                .resolve("0.0.0")
+                .resolve("1.0.0")
                 .resolve("azure.functions.bir");
         
         Path breLibPath = TEST_DISTRIBUTION_PATH
@@ -301,7 +301,7 @@ public class DistributionArtifactCheckTest {
                 .resolve("azure.functions");
         
         Assert.assertTrue(Files.exists(birPath));
-        Assert.assertTrue(Files.exists(breLibPath.resolve("ballerinax-azure.functions-0.0.0.jar")));
+        Assert.assertTrue(Files.exists(breLibPath.resolve("ballerinax-azure.functions-1.0.0.jar")));
         Assert.assertNotNull(TestUtils.findFileOrDirectory(breLibPath, "azurefunctions-extension-"));
         Assert.assertTrue(Files.exists(bbePath));
         Assert.assertTrue(Files.exists(docsPath));
