d1522af6dd90c5001f2a75e96b1cd9691b260467
lnash94 <sumudunissanke@gmail.com>
2020-11-03 10:51:19 +0530

    Add test for checking the openapi main commands work properly

---
+++ ballerina-test/src/test/java/org/ballerinalang/distribution/test/OpenAPIArtifactBuildTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.distribution.test;
+
+import org.apache.commons.io.FileUtils;
+import org.ballerinalang.distribution.utils.TestUtils;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.ballerinalang.distribution.utils.TestUtils.DISTRIBUTIONS_DIR;
+import static org.ballerinalang.distribution.utils.TestUtils.MAVEN_VERSION;
+
+/**
+ * Tests related to artifact generation.
+ */
+public class OpenAPIArtifactBuildTest {
+
+    @DataProvider(name = "distribution-provider")
+    public Object[][] distributionNameProvider() {
+        return new Object[][]{
+                {"ballerina-" + MAVEN_VERSION}
+        };
+    }
+
+    @BeforeClass
+    public void setupDistributions() throws IOException {
+        TestUtils.cleanDistribution();
+        for (Object[] dist : distributionNameProvider()) {
+            String distName = (String) dist[0];
+            TestUtils.prepareDistribution(DISTRIBUTIONS_DIR.resolve(distName + ".zip"));
+        }
+    }
+
+    @Test(dataProvider = "distribution-provider")
+    public void buildOpenAPIToBallerinaTest(String distributionFileName) throws IOException, InterruptedException {
+        Path testResource = Paths.get("/openapi");
+        List<String> buildArgs = new LinkedList<>();
+        buildArgs.add("-i");
+        buildArgs.add("petstore.yaml");
+        boolean successful = TestUtils.executeOpenAPI(distributionFileName, TestUtils.getResource(testResource),
+                buildArgs);
+        Assert.assertTrue(successful);
+        Assert.assertTrue(Files.exists(TestUtils.getResource(testResource).resolve("petstore-service.bal")));
+        Assert.assertTrue(Files.exists(TestUtils.getResource(testResource).resolve("petstore-client.bal")));
+        TestUtils.deleteGeneratedFiles("petstore");
+    }
+
+    @Test(dataProvider = "distribution-provider")
+    public void buildBallerinaToOpenAPITest(String distributionFileName) throws IOException, InterruptedException {
+        Path testResource = Paths.get("/openapi");
+        List<String> buildArgs = new LinkedList<>();
+        buildArgs.add("-i");
+        buildArgs.add("petstore.bal");
+        boolean successful = TestUtils.executeOpenAPI(distributionFileName, TestUtils.getResource(testResource),
+                buildArgs);
+        Assert.assertTrue(successful);
+        Assert.assertTrue(Files.exists(TestUtils.getResource(testResource).resolve("hello-openapi.yaml")));
+        TestUtils.deleteGeneratedFiles("hello-openapi.yaml");
+    }
+
+    @AfterClass
+    public void cleanUp() throws IOException {
+        TestUtils.cleanDistribution();
+    }
+}
