64761ccbf4be89f903048142e8eed7ef635cf1c2
lnash94 <sumudunissanke@gmail.com>
2021-05-04 11:04:38 +0530

    Update OpenAPI version

--- ballerina-test/src/test/java/org/ballerinalang/distribution/test/OpenAPIDistributionArtifactCheck.java
+++ ballerina-test/src/test/java/org/ballerinalang/distribution/test/OpenAPIDistributionArtifactCheck.java
@@ -52,7 +52,7 @@ public class OpenAPIDistributionArtifactCheck {
                 .resolve("cache")
                 .resolve("ballerina")
                 .resolve("openapi")
-                .resolve("0.8.0")
+                .resolve("0.9.0-beta1")
                 .resolve("bir");
 
         Path jarPath = TEST_DISTRIBUTION_PATH
@@ -61,7 +61,7 @@ public class OpenAPIDistributionArtifactCheck {
                 .resolve("cache")
                 .resolve("ballerina")
                 .resolve("openapi")
-                .resolve("0.8.0")
+                .resolve("0.9.0-beta1")
                 .resolve("java11");
 
         Path balaPath = TEST_DISTRIBUTION_PATH
@@ -70,7 +70,7 @@ public class OpenAPIDistributionArtifactCheck {
                 .resolve("bala")
                 .resolve("ballerina")
                 .resolve("openapi")
-                .resolve("0.8.0")
+                .resolve("0.9.0-beta1")
                 .resolve("java11")
                 .resolve("platform")
                 .resolve("java11");
