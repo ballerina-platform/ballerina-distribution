89bec14ea1bfc279cfb005dd338564ff209512d4
Thevakumar-Luheerathan <Luhee27@gmail.com>
2023-08-18 12:01:24 +0530

    [DON'T MERGE]

--- project-api-tests/src/test/java/org/ballerina/projectapi/MavenCustomRepoTest.java
+++ project-api-tests/src/test/java/org/ballerina/projectapi/MavenCustomRepoTest.java
@@ -64,6 +64,7 @@ public class MavenCustomRepoTest {
     @BeforeClass()
     public void setUp() throws IOException {
         TestUtils.setupDistributions();
+        System.out.println("Username is ++" + System.getenv("packageUser"));
         actualHomeDirectory = Paths.get(System.getProperty("user.home")).resolve(".ballerina");
         actualHomeDirectoryClone = Files.createTempDirectory("bal-test-integration-packaging-home-")
                 .resolve(".ballerina");
