029bd5d6d7022050813765d6697cbf4a9bf42dcd
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2024-08-21 18:41:39 +0530

    Move BuildNewCommandTest to project api

--- project-api-tests/src/test/java/org/ballerina/projectapi/TestUtils.java
+++ project-api-tests/src/test/java/org/ballerina/projectapi/TestUtils.java
@@ -108,6 +108,12 @@ public class TestUtils {
         return executeCommand("search", distributionName, sourceDirectory, args, envProperties);
     }
 
+    public static Process executeNewCommand(String distributionName, Path sourceDirectory,
+                                            List<String> args, Map<String, String> envProperties)
+            throws IOException, InterruptedException {
+        return executeCommand("new", distributionName, sourceDirectory, args, envProperties);
+    }
+
     /**
      * Clean and setup the distribution.
      *
